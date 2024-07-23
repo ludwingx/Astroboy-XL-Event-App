594b8c7d375dd2ba6023b0ccafbf11ad37245478
ludwingx <ludwingytx@gmail.com>
2024-07-23 10:39:08 -0400

    page clients table add

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,20 +1,45 @@
-  import prisma from "@/lib/db";
-  async function Clients() {
-    const clients = await prisma.clients.findMany();
-    console.log(clients);
-    return (
-      <div>
-        <h1 >Clients</h1>
-        <ul>
-        {clients.map((client) => (
-            <li key={client.id}>
-              {client.name} {client.lastname} - {client.category} - {client.merch}
-            </li>
-          ))}
-        </ul>
+import prisma from "@/lib/db";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box } from "@mui/material";
 
+async function Clients() {
+  const clients = await prisma.clients.findMany();
+  console.log(clients);
 
-      </div>
-    );
-  }
-  export default Clients
+  return (
+    <div>
+      <Box mt={4} mb={2} sx={{ textAlign: "center" }}>
+        <h1>LISTA DE DATOS PERSONAS REGISTRADAS</h1>
+      </Box>
+      <TableContainer component={Paper} sx={{ maxWidth: '80%', margin: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ width: '5%' }}>ID</TableCell>
+              <TableCell sx={{ width: '15%' }}>Name</TableCell>
+              <TableCell sx={{ width: '15%' }}>Last Name</TableCell>
+              <TableCell sx={{ width: '20%' }}>Email</TableCell>
+              <TableCell sx={{ width: '15%' }}>Phone</TableCell>
+              <TableCell sx={{ width: '15%' }}>Category</TableCell>
+              <TableCell sx={{ width: '15%' }}>Merch</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {clients.map((client) => (
+              <TableRow key={client.id}>
+                <TableCell>{client.id}</TableCell>
+                <TableCell>{client.name}</TableCell>
+                <TableCell>{client.lastname}</TableCell>
+                <TableCell>{client.email}</TableCell>
+                <TableCell>{client.phone}</TableCell>
+                <TableCell>{client.category}</TableCell>
+                <TableCell>{client.merch}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
+
+export default Clients;
