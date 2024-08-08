95dbecb4d388cbdf7a9525c22a595ad7b58924cc
ludwingx <ludwingytx@gmail.com>
2024-08-07 22:22:18 -0400

    add login btn in homepage

--- src/app/entradas/Entradas.tsx
+++ src/app/entradas/Entradas.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import styles from './tickets.module.css';
-import { ticketsLinks } from '@/utils/ticketsLinks';
-
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import styles from "./tickets.module.css";
+import { ticketsLinks } from "@/utils/ticketsLinks";
+import { Box, Button} from "@mui/material";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 export default function Tickets() {
-  const [category, setCategory] = useState<string>('');
+  const [category, setCategory] = useState<string>("");
   const router = useRouter();
 
   const handleCategoryClick = (category: string) => {
@@ -14,16 +16,19 @@ export default function Tickets() {
     // Redirige a la página de entradas específica
     router.push(`/entradas/${category}`);
   };
-  const title = 'ASTROBOY XL LISTENING PARTY';
-  const subtitle = 'CASA GRANDE / MOANA';
-  const subtitle2 = '31 AGOSTO';
+  const title = "ASTROBOY XL LISTENING PARTY";
+  const subtitle = "CASA GRANDE / MOANA";
+  const subtitle2 = "31 AGOSTO";
   return (
     <>
       <div className={styles.container}>
         <div className={styles.textContainer}>
           <h1 className={styles.title}>{title}</h1>
-          <h4 className={styles.subtitle}>{subtitle}</h4>
-          <h4 className={styles.subtitle2}>{subtitle2}</h4>
+
+          <h4 className={styles.subtitle}><LocationOnIcon sx={{ color: 'red', marginBottom: '0.5rem'}}  /> {subtitle}</h4>
+
+
+          <h4 className={styles.subtitle2}><CalendarMonthIcon sx={{ marginBottom: '0.5rem'}}/> {subtitle2}</h4>
         </div>
         <h1 className={styles.title2}>SELECCIONA TU ENTRADA</h1>
         <div className={styles.buttonsContainer}>
@@ -37,6 +42,19 @@ export default function Tickets() {
             </button>
           ))}
         </div>
-      </div></>
+        <div className={styles.textContainer}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <h2 className={styles.title3}>
+              Si ya estas registrado, inicia sesion para ver tu entrada
+            </h2>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Button variant="contained" className={styles.button2}>
+              Iniciar Sesion
+            </Button>
+          </Box>
+        </div>
+      </div>
+    </>
   );
 }
